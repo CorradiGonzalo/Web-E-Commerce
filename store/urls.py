98242da3841d543baa0d06ba771bc7d26988355f
GonzaloCorradi <corradigonzalo9@gmail.com>
2026-01-07 22:32:20 -0300

    actualizacion el urls, empezando con detail

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('producto/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def home(request):
     products = Product.objects.filter(is_active=True)
     return render(request, 'store/home.html', {'products': products})
+
+def product_detail(request, slug):
+    #BUSCAMOS PRODUCTO POR URL
+    product = get_object_or_404(Product, slug=slug, is_active=True)
+    return render(request, 'store/detail.html', {'product': product})
